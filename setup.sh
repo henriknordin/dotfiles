a8c118c14d14fadced26d95904e9dd02fd349540
Henrik Nordin <nordhenr@gmail.com>
2016-08-28 19:34:29 +0200

    Renamed directory

--- setup.sh
+++ setup.sh
@@ -26,19 +26,19 @@ create_link() {
 main() {
     dot="$(cd "$(dirname "$0")"; pwd)"
 
-    create_link $dot/.config/git .config/git
+    create_link $dot/config/git .config/git
   
     curl -fLo "$dot/.config/vim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    create_link $dot/.config/vim .config/vim
+    create_link $dot/config/vim .config/vim
 
     curl -fLo "$dot/.config/nvim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    create_link $dot/.config/nvim .config/nvim
+    create_link $dot/config/nvim .config/nvim
 
-    create_link $dot/.config/termite .config/termite
+    create_link $dot/config/termite .config/termite
 
-    create_link $dot/.config/tmux .config/tmux
+    create_link $dot/config/tmux .config/tmux
 }
 
 main "$@"
