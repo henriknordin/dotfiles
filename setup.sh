cdd7f0a05bf8d46eb3d81a1b100351eec7bf9c80
Henrik Nordin <nordhenr@gmail.com>
2016-08-30 23:27:36 +0200

    Bug fixes in setup following directory restructuring

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+#
+# Setup of configuration provided in the repo.
+
+PROGNAME=$(basename $0)
 
 create_link() {
 
@@ -8,31 +12,45 @@ create_link() {
     if [[ "$origin" != "$(readlink $target)" ]]; then
         # Check if link should be created
         if [[ ! -d "$target" ]]; then
-            echo "Creating symbolic link from $origin to $target"
-            ln -s $origin $target
-            if [[ "$?" -ne 0 ]]; then
-                echo "Unable to create symbolic link from $origin to $target"
-                exit 1
+            if ! ln -s $origin $target ; then
+                error_exit "$LINENO: Failed creating symbolic from $origin to $target"
             fi
+            echo "Creating symbolic link from $origin to $target"
+        else 
+            error_exit "$LINENO: $target already exists and points at $(readlink $target)"
         fi
-    else
-        echo "Link is already correct"
+#    else
+#        echo "Link $target is already correct"
     fi
 }
 
 
-# Main
+###################################################################
+# Print an error message to stderr and terminate with exit code 1
+# Globals:
+#   PROGNAME
+# Arguments:
+#   $1 Optional error message 
+# Returns:
+#   None
+###################################################################
+error_exit() {
+    echo "${PROGNAME}: ${1:-"Unknown error"}" 1>&2
+    exit 1
+}
 
+
+# Main
 main() {
     dot="$(cd "$(dirname "$0")"; pwd)"
 
     create_link $dot/config/git .config/git
   
-    curl -fLo "$dot/.config/vim/autoload/plug.vim" --create-dirs \
+    curl -fLo "$dot/config/vim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     create_link $dot/config/vim .config/vim
 
-    curl -fLo "$dot/.config/nvim/autoload/plug.vim" --create-dirs \
+    curl -fLo "$dot/config/nvim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     create_link $dot/config/nvim .config/nvim
 
